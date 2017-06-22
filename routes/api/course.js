a2e82f951de4fb3542ad39677caa69274ea652a9
sMailund <simen.m.s@hotmail.com>
2017-06-22 12:47:45 +0200

    improved sample output

--- routes/api/course.js
+++ routes/api/course.js
@@ -5,10 +5,21 @@ var router = express.Router();
 //TODO: apien burde heller sende ting i dette formatet,
 //enn sånn det kommer fra scraper
 var output = {
-  title: "gruppe 1 MIT",
-  day: 3,
-  start: "2014-06-11T14:15", //TODO: hvordan dealer jeg med at jeg ikke er opptatt av eksakt dato?
-  end: "2014-06-11T16:00"
+  "Fellesundervisning": [
+      {
+         "title": "Forelesninger",
+         "time": [
+            {
+               "start": "2017-05-03T14:15",
+               "end": "2017-05-03T16:00"
+            },
+            {
+               "start": "2017-05-04T10:15",
+               "end": "2017-05-04T12:00"
+            }
+         ]
+      }
+   ]
 };
 
 
