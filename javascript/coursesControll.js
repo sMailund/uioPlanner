299bb30e903f0566f385c61963356a5636df6c0b
sMailund <simen.m.s@hotmail.com>
2017-06-28 21:04:48 +0200

    read course data and display on page
    early version, only for one course

--- javascript/coursesControll.js
+++ javascript/coursesControll.js
@@ -1,19 +1,115 @@
-var jquery = require('jquery');
+/*jshint esversion: 6 */
+
+//TODO: untangle the spaghetti
+
+let jquery = require('jquery');
+let http = require('http');
+
+let courses = [];
+let numberOfCourses = 0;
 
 //TODO: er det en bedre måte å legge til html?
 $(document).ready(function() {
   $("#addCourse").click(function() {
-    $("#courses").append(
-    "<div>" +
-      "hello" +
-      '<button id="addedbutton">world</button>' +
-    "</div>");
+    courses[0] = _getJSON($('#courseSearchBox').val());
   });
-  $("#courses").on("click", "button#addedbutton", function() {
+  //add click handler to button in added div
+  $("#courses").on("click", "button#addedbutton" + 1, function() {
     external();
   });
 });
 
-function external() {
-  alert("it works");
+//TODO: error handling
+
+function _getJSON(course) {
+  let url = '/api/course?code=' + $('#courseSearchBox').val();
+  http.get(url, function(res){
+    let body = '';
+
+    res.on('data', function(chunk){
+        body += chunk;
+    });
+
+    res.on('end', function(){
+        let response = JSON.parse(body);
+        numberOfCourses++;
+        courses[numberOfCourses] = response; //add json to courses
+        _createCourseBox(numberOfCourses);
+    });
+  }).on('error', function(e){
+        console.log("Got an error: ", e);
+  });
+}
+
+function _createCourseBox(number) {
+
+  //create new div
+  $("#courses").append(
+    $('<div />')
+    .attr("id", "course" + number)
+      .append(
+        $("<h2 />")
+        .text(courses[number][0].coursecode)
+      )
+      .append(
+        $('<div />')
+        .attr("id", "course" + number + "felles")
+        .append(
+          $('<h3 />')
+          .text("Fellesundervisning")
+        )
+      )
+      .append(
+        $('<div />')
+        .attr("id", "course" + number + "gruppe")
+        .append(
+          $('<h3 />')
+          .text("Gruppeundervisning")
+        )
+      )
+    );
+
+    let fellesDiv = $("#course" + number + "felles");
+    _createActivities(courses[number][0].activities.Fellesundervisning,
+                      number,
+                      fellesDiv);
+
+    let gruppeDiv = $("#course" + number + "gruppe");
+    _createActivities(courses[number][0].activities.Gruppeundervisning,
+                      number,
+                      gruppeDiv);
+
+    //add click handler to button in added div
+    $("#course" + number).on("click", "button#addLecture", function() {
+      external(number);
+    });
+}
+
+function _createActivities(activities, coursenum, div) {
+  let activityNum = 0;
+
+  activities.forEach(function(activity) {
+
+    let checkboxId = "course" + coursenum + "activity" + activityNum++;
+
+    div.append(
+      $('<input />')
+        .attr("type", "checkbox")
+        .attr("id", checkboxId)
+    );
+
+    div.append(
+      $('<label />')
+        .attr("for", checkboxId)
+        .text(activity.title)
+    );
+
+    div.append(
+      $('<br>')
+    );
+  });
+}
+
+function external(number) {
+  console.log(courses[number][0].activities);
 }
