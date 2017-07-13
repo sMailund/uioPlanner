6cf13d1680b4750afc92147e6a03e5f1861ceff7
sMailund <simen.m.s@hotmail.com>
2017-07-13 23:36:09 +0200

    connect through seperate db module

--- routes/api/course.js
+++ routes/api/course.js
@@ -2,36 +2,12 @@
 
 const express = require('express');
 const router = express.Router();
-const pgp = require('pg-promise')();
-
-const db = pgp('postgres://test_user:pleaseIgnroe@localhost:5432/planner');
-
-//test data
-let output = {
-  "Fellesundervisning": [
-      {
-         "title": "Forelesninger",
-         "time": [
-            {
-               "start": "2017-05-03T14:15",
-               "end": "2017-05-03T16:00"
-            },
-            {
-               "start": "2017-05-04T10:15",
-               "end": "2017-05-04T12:00"
-            }
-         ]
-      }
-   ]
-};
-
-
+const connection = require('./dbConnect.js');
 
 router.get('/', function(req, res, next) {
   console.log(req.query.code);
-  db.query("SELECT * FROM courses WHERE course_id = \'$1#\'", [req.query.code])
+  connection.db.query("SELECT * FROM courses WHERE course_id = \'$1#\'", [req.query.code])
   .then(function(result) {
-    //console.log("got result: " + JSON.stringify(result));
     res.json(result);
   }).catch(function(error) {
     console.log("error: " + error);
---
+++ routes/api/dbConnect.js
@@ -0,0 +1,8 @@
+/*jshint esversion: 6 */
+
+const pgp = require('pg-promise')();
+const db = pgp('postgres://test_user:pleaseIgnroe@localhost:5432/planner');
+
+module.exports = {
+  pgp, db
+};
