a13299b0e3603d87e937c03dcf4662844c2824c2
sMailund <simen.m.s@hotmail.com>
2017-07-14 22:21:09 +0200

    remove console.log

--- routes/api/course.js
+++ routes/api/course.js
@@ -5,7 +5,6 @@ const router = express.Router();
 const connection = require('./dbConnect.js');
 
 router.get('/', function(req, res, next) {
-  console.log(req.query.code);
   connection.db.query("SELECT * FROM courses WHERE course_id = \'$1#\'", [req.query.code])
   .then(function(result) {
     res.json(result);
