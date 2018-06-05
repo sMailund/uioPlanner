d9b005629f487b4f496f5497375d5f6acc026f84
Unknown <simen.m.s@hotmail.com>
2018-06-05 18:21:00 +0200

    repair fetching of course-JSON
    the culprit was most likely the missing request headers

--- scraper/courseFetcher.py
+++ scraper/courseFetcher.py
@@ -1,12 +1,29 @@
 import urllib2
 import json
 
+#fetching the JSON does not work without headers, 
+#and will return the regular page if headers are missing
+requestHeaders = {
+    "Accept": "application/json, text/javascript, */*; q=0.01",
+    "Accept-Encoding": "gzip, deflate",
+    "X-Requested-With": "XMLHttpRequest"
+}
+
 #takes a url to a json-source and returns a dictionary of the json
 def fetch(url):
-    fetched = urllib2 \
-        .urlopen(url) \
-        .read()
-    return json.loads(fetched)
+    request = __createRequest(url)
+    response = urllib2.urlopen(request)
+    return json.loads(response.read())
+
+def __createRequest(url):
+    request = urllib2.Request(url)
+    request = __addRequestHeaders(request)
+    return request
+
+def __addRequestHeaders(request):
+    for key, value in requestHeaders.items():
+        request.add_header(key, value)
+    return request
 
 #test url
 #http://www.uio.no/studier/emner/matnat/ifi/INF1510/v17/timeplan/index.html?action=course-schedule
--- scraper/urlBuilder.py
+++ scraper/urlBuilder.py
@@ -1,11 +1,21 @@
+import calendar
+import time
 
 baseurl = "http://www.uio.no"
 schedulePath = "/timeplan/index.html"
 allCoursesPath = schedulePath + "?action=course-schedule"
+timeparam = 't'
 
 def buildJsonURL(href, semester):
     path = href.split("/index.html")[0]
     return baseurl \
         + path \
         + ("/%s" % semester) \
-        + allCoursesPath
+        + allCoursesPath \
+        + '&' \
+        + timeparam \
+        + '=' \
+        + getEpochTimeNow() #parameter t must be set with current epoch time for json link to work
+
+def getEpochTimeNow():
+    return str(calendar.timegm(time.gmtime()))
